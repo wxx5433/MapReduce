e8077cf09056070a9080c6e506c20fa3b4c2161b
wxx5433 <wxx5433@gmail.com>
2014-11-11 17:56:25 -0500

    move remote split operation from DFS client to this file

--- src/fileSplit/RemoteSplitOperator.java
+++ src/fileSplit/RemoteSplitOperator.java
@@ -1,31 +1,54 @@
 package fileSplit;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.util.List;
 
+import node.NodeID;
 import dataNode.DataNodeService;
+import dfs.Service;
 
 public class RemoteSplitOperator {
-
-	public void writeBlock(FileSplit fb, List<String> contents) throws Exception {
-		// write to all servers. 
-		for (String host: fb.getHosts()) {
-			// lookup dataNode's rmi service
-			String service = "rmi://" + host + "/DataNodeService";
+	/**
+	 * Write splits to several remote dataNodes
+	 * @param dataNodes The dataNodes to write the file split
+	 * @param fileName The file's name which the split belong to 
+	 * @param blockIndex The block index for the split 
+	 * @param contents The split's contents
+	 * @throws Exception
+	 */
+	public void writeSplit(Iterable<NodeID> dataNodes, String fileName, 
+			int blockIndex, List<String> contents) throws Exception {
+		for (NodeID dataNodeID: dataNodes) {
+			DataNodeService dataNodeService = Service.getDataNodeService(dataNodeID);
+			String dataNodePath = dataNodeID.getRootPath() + File.separator + 
+					fileName + "_" + blockIndex;
 			try {
-				DataNodeService dataNodeService = (DataNodeService)Naming.lookup(service);
-				dataNodeService.writeBlock(fb.getPath(host), contents);
-			} catch (NotBoundException e) {
+				dataNodeService.writeBlock(dataNodePath, contents);
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public String[] readBlock(FileSplit fb) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> readSplit(FileSplit split, 
+			String localPath, String targetName) throws IOException {
+		NodeID dataNodeID = split.getOneHost();
+		DataNodeService dataNodeService = Service.getDataNodeService(dataNodeID);
+		List<String> lines = null;
+		try {
+			// read split from dataNode. It's an RMI 
+			lines = dataNodeService.readBLock(
+					split.getPath(dataNodeID.toString()));
+		} catch (Exception e) {
+			System.out.println("Fail to download split from dataNode");
+			e.printStackTrace();
+		}
+		return lines;
 	}
 
 }
