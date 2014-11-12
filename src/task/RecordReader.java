9bf73edeef7101868c99acfddf96aff35738c861
wxx5433 <wxx5433@gmail.com>
2014-11-11 20:25:22 -0500

    change RecordReader to abstract class and define its iterface

--- src/task/RecordReader.java
+++ src/task/RecordReader.java
@@ -1,83 +1,16 @@
 package task;
 
-import java.io.RandomAccessFile;
+import fileSplit.FileSplit;
 
-public class RecordReader {
-	private String fileName;
-	private int recordLen;
+public abstract class RecordReader<KEYIN, VALUEIN> {
 
-	public RecordReader(String fn, int len) {
-		fileName = fn;
-		recordLen = len;
-	}
+	public abstract void initialize(FileSplit split, String host);
 
-	public int getRecordNum() {
-		long num = 0;
-		try {
-			RandomAccessFile raf;
-			raf = new RandomAccessFile(fileName, "r");
-			while (raf.readLine() != null)
-				num++;
-			raf.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return (int) num;
-	}
+	public abstract boolean nextKeyValue();
 
-	public String[][] getKeyValuePairs(int partitionIndex, int partitionSize) {
-		RandomAccessFile raf = null;
-		String[][] pairs = null;
-		try {
-			int count = 0;
-			raf = new RandomAccessFile(fileName, "r");
+	public abstract KEYIN getCurrentKey();
 
-			while (count < partitionIndex) {
-				count++;
-				raf.readLine();
-			}
+	public abstract VALUEIN getCurrentValue();
 
-			pairs = new String[partitionSize][2];
-			String record;
-
-			for (int i = 0; i < partitionSize; i++) {
-				record = raf.readLine();
-				if (record == null)
-					break;
-
-				pairs[i][0] = ((Integer) (count + i)).toString();
-				pairs[i][1] = record;
-			}
-
-			raf.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return pairs;
-	}
-
-	public boolean nextKeyValue() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public String getCurrentKey() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getCurrentValue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public boolean nextKey() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public Iterable<String> getValues() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public abstract void close();
 }
