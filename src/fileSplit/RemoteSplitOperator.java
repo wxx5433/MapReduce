af008a5ded71d06c2bca611a916f6866010397fd
zhouyejoe <zhouyejoe@gmail.com>
2014-11-16 23:26:09 -0500

    Add readblock function to read the output of mapper

--- src/fileSplit/RemoteSplitOperator.java
+++ src/fileSplit/RemoteSplitOperator.java
@@ -1,11 +1,7 @@
 package fileSplit;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.util.List;
 
 import node.NodeID;
@@ -15,18 +11,24 @@ import dfs.Service;
 public class RemoteSplitOperator {
 	/**
 	 * Write splits to several remote dataNodes
-	 * @param dataNodes The dataNodes to write the file split
-	 * @param fileName The file's name which the split belong to 
-	 * @param blockIndex The block index for the split 
-	 * @param contents The split's contents
+	 * 
+	 * @param dataNodes
+	 *            The dataNodes to write the file split
+	 * @param fileName
+	 *            The file's name which the split belong to
+	 * @param blockIndex
+	 *            The block index for the split
+	 * @param contents
+	 *            The split's contents
 	 * @throws Exception
 	 */
-	public void writeSplit(Iterable<NodeID> dataNodes, String fileName, 
+	public void writeSplit(Iterable<NodeID> dataNodes, String fileName,
 			int blockIndex, List<String> contents) throws Exception {
-		for (NodeID dataNodeID: dataNodes) {
-			DataNodeService dataNodeService = Service.getDataNodeService(dataNodeID);
-			String dataNodePath = dataNodeID.getRootPath() + File.separator + 
-					fileName + "_" + blockIndex;
+		for (NodeID dataNodeID : dataNodes) {
+			DataNodeService dataNodeService = Service
+					.getDataNodeService(dataNodeID);
+			String dataNodePath = dataNodeID.getRootPath() + File.separator
+					+ fileName + "_" + blockIndex;
 			try {
 				dataNodeService.writeSplit(dataNodePath, contents);
 			} catch (Exception e) {
@@ -35,15 +37,31 @@ public class RemoteSplitOperator {
 		}
 	}
 
-	public List<String> readSplit(FileSplit split, 
-			String localPath, String targetName) throws IOException {
+	public List<String> readSplit(FileSplit split, String localPath,
+			String targetName) throws IOException {
 		NodeID dataNodeID = split.getOneDataNode();
-		DataNodeService dataNodeService = Service.getDataNodeService(dataNodeID);
+		DataNodeService dataNodeService = Service
+				.getDataNodeService(dataNodeID);
 		List<String> lines = null;
 		try {
-			// read split from dataNode. It's an RMI 
-			lines = dataNodeService.readSplit(
-					split.getPath(dataNodeID.toString()));
+			// read split from dataNode. It's an RMI
+			lines = dataNodeService.readSplit(split.getPath(dataNodeID
+					.toString()));
+		} catch (Exception e) {
+			System.out.println("Fail to download split from dataNode");
+			e.printStackTrace();
+		}
+		return lines;
+	}
+
+	public List<String> readBlock(NodeID dataNodeID, String localPath,
+			String targetName) throws IOException {
+		DataNodeService dataNodeService = Service
+				.getDataNodeService(dataNodeID);
+		List<String> lines = null;
+		try {
+			// read split from dataNode. It's an RMI
+			lines = dataNodeService.readSplit(localPath);
 		} catch (Exception e) {
 			System.out.println("Fail to download split from dataNode");
 			e.printStackTrace();
