0bef62f7fdb05acbbf7e053cd7c2f3338bdb0ab4
wxx5433 <wxx5433@gmail.com>
2014-11-19 18:45:15 -0500

    add comments

--- src/task/TaskInProgress.java
+++ src/task/TaskInProgress.java
@@ -3,6 +3,12 @@ package task;
 public class TaskInProgress {
 	private int id;
 	
+	/**
+	 * Id within the job. 
+	 * It is the index of maps array in JobInProgress.
+	 * Passed in when new TaskInProgress
+	 * @return
+	 */
 	public int getTIPId() {
 		return this.id;
 	}
