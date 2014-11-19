030abfe71e40a47c199f9c2fbeb8aaa7329667e3
wxx5433 <wxx5433@gmail.com>
2014-11-19 18:25:41 -0500

    add createSplits

--- src/job/JobInProgress.java
+++ src/job/JobInProgress.java
@@ -1,14 +1,22 @@
 package job;
 
+import configuration.Configuration;
+
 import java.io.IOException;
+import java.rmi.RemoteException;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 import configuration.MyConfiguration;
+import dfs.Service;
+import fileSplit.FileSplit;
 import jobtracker.JobTracker;
+import nameNode.NameNodeService;
 import node.Node;
+import node.NodeID;
 import task.TaskInProgress;
 
 public class JobInProgress {
@@ -22,7 +30,7 @@ public class JobInProgress {
 		}
 	}
 
-//	static final Log LOG = LogFactory.getLog(JobInProgress.class);
+	//	static final Log LOG = LogFactory.getLog(JobInProgress.class);
 	Job job = null;
 	TaskInProgress maps[] = new TaskInProgress[0];
 	TaskInProgress reduces[] = new TaskInProgress[0];
@@ -46,13 +54,16 @@ public class JobInProgress {
 	final JobTracker jobtracker;
 
 	// NetworkTopology Node to the set of TIPs
-	Map<Node, List<TaskInProgress>> nonRunningMapCache;
+	//	Map<Node, List<TaskInProgress>> nonRunningMapCache;
 
 	// Map of NetworkTopology Node to set of running TIPs
 	Map<Node, Set<TaskInProgress>> runningMapCache;
 
 	// A set of non-local running maps
-	Set<TaskInProgress> nonLocalRunningMaps;
+	Set<TaskInProgress> nonRunningMaps;
+
+	// All failed map tasks
+	Set<TaskInProgress> failedMaps;
 
 	// A list of non-running reduce TIPs
 	Set<TaskInProgress> nonRunningReduces;
@@ -60,29 +71,37 @@ public class JobInProgress {
 	// A set of running reduce TIPs
 	Set<TaskInProgress> runningReduces;
 
-	// List<TaskCompletionEvent> taskCompletionEvents;
+
+	private volatile boolean tasksInit;
+	private volatile boolean jobComplete;
 
 	long startTime;
 	long launchTime;
 	long finishTime;
 
-	private MyConfiguration conf;
+	//	private MyConfiguration conf;
+	private JobConf conf;
 	private JobID jobId;
+
+	private NameNodeService nameNodeService;
 
 	/**
 	 * Create an almost empty JobInProgress, which can be used only for tests
 	 */
-	protected JobInProgress(JobID jobid, MyConfiguration conf,
+	protected JobInProgress(JobID jobid, JobConf conf,
 			JobTracker tracker) throws IOException {
+		this.tasksInit= false;
+		this.jobComplete = false;
 		this.conf = conf;
 		this.jobId = jobid;
 		// this.numMapTasks = conf.getNumMapTasks();
-		// this.numReduceTasks = conf.getNumReduceTasks();
+		this.numReduceTasks = conf.getNumReduceTasks();
 		this.jobtracker = tracker;
-		// this.failedMaps = new TreeSet<TaskInProgress>(failComparator);
-		this.nonLocalRunningMaps = new LinkedHashSet<TaskInProgress>();
-		// this.nonRunningReduces = new TreeSet<TaskInProgress>(failComparator);
+		this.failedMaps = new TreeSet<TaskInProgress>();//failComparator);
+		this.nonRunningMaps = new LinkedHashSet<TaskInProgress>();
+		this.nonRunningReduces = new TreeSet<TaskInProgress>();//failComparator);
 		this.runningReduces = new LinkedHashSet<TaskInProgress>();
+		this.nameNodeService = getNameNodeService();
 	}
 
 	/**
@@ -90,27 +109,35 @@ public class JobInProgress {
 	 * split-computation doesn't block anyone.
 	 */
 	public synchronized void initTasks() throws IOException,
-			KillInterruptedException {
-		// if (tasksInited || isComplete()) {
-		// return;
-		// }
+	KillInterruptedException {
+		// return if the job has already initialize tasks or finished. 
+		if (tasksInit || isComplete()) {
+			return;
+		}
 
-//		LOG.info("Initializing " + jobId);
-		final long startTimeFinal = this.startTime;
+
+		System.out.println("Initializing " + jobId);
+		//		final long startTimeFinal = this.startTime;
 		// log job info as the user running the job
 
-		//
 		// read input splits and create a map per a split
-		//
-		// TaskSplitMetaInfo[] splits = createSplits(jobId);
-		// numMapTasks = splits.length;
+		FileSplit[] splits = createSplits();
+		numMapTasks = splits.length;
+		maps = new TaskInProgress[numMapTasks];
+		for (int i = 0; i < numMapTasks; ++i) {
+			// TODO Auto-generated method stub
+			// need to pass arguments here!!!!!!!!!!!!!!
+			maps[i] = new TaskInProgress();
+			nonRunningMaps.add(maps[i]);
+		}
+		System.out.println("Successfully initialized all map tasks for job: " + jobId);
 		//
 		// jobtracker.getInstrumentation().addWaitingMaps(getJobID(),
 		// numMapTasks);
 		// jobtracker.getInstrumentation().addWaitingReduces(getJobID(),
 		// numReduceTasks);
 		//
-		// maps = new TaskInProgress[numMapTasks];
+		//		 maps = new TaskInProgress[numMapTasks];
 		// for (int i = 0; i < numMapTasks; ++i) {
 		// inputLength += splits[i].getInputDataLength();
 		// maps[i] = new TaskInProgress(jobId, jobFile, splits[i], jobtracker,
@@ -122,20 +149,93 @@ public class JobInProgress {
 		// set the launch time
 		// this.launchTime = jobtracker.getClock().getTime();
 
-		//
 		// Create reduce tasks
-		//
-		this.reduces = new TaskInProgress[numReduceTasks];
+		reduces = new TaskInProgress[numReduceTasks];
 		for (int i = 0; i < numReduceTasks; i++) {
 			// reduces[i] = new TaskInProgress(jobId, jobFile, numMapTasks, i,
 			// jobtracker, conf, this, numSlotsPerReduce);
+			// TODO Auto-generated method stub
+			// need to pass in arguments here!!!!!!!!
+			reduces[i] = new TaskInProgress();
 			nonRunningReduces.add(reduces[i]);
 		}
+		System.out.println("Successfully initialized all reduce tasks for job: " + jobId);
 
-		// Log the number of map and reduce tasks
-//		LOG.info("Job " + jobId + " initialized successfully with "
-//				+ numMapTasks + " map tasks and " + numReduceTasks
-//				+ " reduce tasks.");
+		System.out.println("Job " + jobId + " initialized successfully with "
+				+ numMapTasks + " map tasks and " + numReduceTasks
+				+ " reduce tasks.");
+	}
+
+	/**
+	 * get a new Map task
+	 */
+	public TaskInProgress getNewMap() {
+
+	}
+
+	/**
+	 * get a new Reduce task
+	 * @return
+	 */
+	public TaskInProgress getNewReduce() {
+
+	}
+
+	/**
+	 * Schedule a map task
+	 */
+	public void scheduleMap() {
+
+	}
+
+	/**
+	 * Schedule a reduce tasks
+	 */
+	public void scheduleReduce() {
+
+	}
+
+	/**
+	 * add to fail Map map
+	 * @param mapTask
+	 */
+	public synchronized void failMap(TaskInProgress mapTask) {
+
+	}
+
+	/**
+	 * add to fail Reduce map
+	 */
+	public synchronized void failReduce() {
+
+	}
+
+	public void setJobCompelete() {
+		this.jobComplete = true;
+	}
+
+	public boolean isComplete() {
+		return this.jobComplete;
+	}
+
+	/**
+	 * Contact nameNode and get all the FileSplits
+	 * @return
+	 */
+	private FileSplit[] createSplits() {
+		String inputPath = conf.getInputPath();
+		try {
+			return nameNodeService.getAllSplits(inputPath);
+		} catch (RemoteException e) {
+			System.out.println("Fail to get file splits from nameNode when initializing tasks");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private NameNodeService getNameNodeService() {
+		NodeID nameNodeId = new NodeID(Configuration.masterIP, Configuration.masterPort);
+		return Service.getNameNodeService(nameNodeId);
 	}
 
 }
