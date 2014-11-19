b9208c5c0dd93ef8f1c36a233782f971acf1be1e
wxx5433 <wxx5433@gmail.com>
2014-11-19 18:59:38 -0500

    add API to Tasktracker

--- src/tasktracker/TaskTracker.java
+++ src/tasktracker/TaskTracker.java
@@ -1,5 +1,16 @@
 package tasktracker;
 
-public class TaskTracker implements TaskTrackerInterface {
+import node.NodeID;
 
+public class TaskTracker implements TaskTrackerInterface {
+	private NodeID nodeId;
+	
+	/**
+	 * JobInProgress need node information to allocate tasks to 
+	 * dataNode contains the file split.
+	 * @return
+	 */
+	public NodeID getNodeId() {
+		return this.nodeId;
+	}
 }
