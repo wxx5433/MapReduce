45a8f2b1f8a23003ab66c7348e5e60fb3087c961
zhouyejoe <zhouyejoe@gmail.com>
2014-11-21 17:41:34 -0500

    fix problems in MyToolRunner

--- src/tool/MyToolRunner.java
+++ src/tool/MyToolRunner.java
@@ -1,18 +1,9 @@
 package tool;
 
-import configuration.Configuration;
 
 public class MyToolRunner {
 
-	public static int run(Configuration conf, MyTool tool, String[] args)
-			throws Exception {
-		if (conf == null) {
-			conf = new Configuration();
-		}
-		return tool.run(args);
-	}
-
 	public static int run(MyTool tool, String[] args) throws Exception {
-		return run(tool, args);
+		return tool.run(args);
 	}
 }
